1191c25746bf2dc618ebbf3a709985e108b0962d
Snootic <kaik.eu19@hotmail.com>
2023-09-11 20:27:06 -0300

    Initial commit

---
+++ TelaInicial.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import ttkbootstrap as tkb
+from tkinter import ttk
+from tkinter import scrolledtext
+
+#TODO: Tela inicial contendo fields para inserção de dados
+# tela para visualização de tabelas
+# tela para visualização de gráficos
+# botoes/menus para importação de arquivos csv e xsls
