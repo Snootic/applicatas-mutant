5edd56853aa35b5bcc417be54dc84131a9cc501d
Snootic <kaik.eu19@hotmail.com>
2023-09-11 21:31:02 -0300

    importar menu adicionado

--- TelaInicial.py
+++ TelaInicial.py
@@ -7,3 +7,24 @@ from tkinter import scrolledtext
 # tela para visualização de tabelas
 # tela para visualização de gráficos
 # botoes/menus para importação de arquivos csv e xsls
+
+home = tkb.Window()
+home.geometry('600x400')
+home.title("Peraeque - HOME")
+
+#menu
+menu = tk.Menu(home)
+importar_menu = tk.Menu(menu, tearoff=False)
+importar_menu.add_command(label = 'Importar CSV', command = lambda: print('alo'))
+importar_menu.add_command(label = 'Importar XSLS', command = lambda: print('yes'))
+menu.add_cascade(label='Importar', menu = importar_menu)
+
+#Abas
+notebook = tkb.Notebook(home)
+
+
+#configurações tela
+home.configure(menu = menu)
+
+#rodar tela
+home.mainloop()
