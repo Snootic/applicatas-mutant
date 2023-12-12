d3502f14f024d4de28b6924609ddf42be78489b4
Snootic <kaik.eu19@hotmail.com>
2023-12-12 20:06:15 -0300

    fix: comboboxes values

--- telas/inicio.py
+++ telas/inicio.py
@@ -68,7 +68,6 @@ class inicio:
         tabelas = tabela()
         sqlite = pareto()
         
-        ep.abrir_tabela['value'] = tabelas.getTabelas('pareto')
         # Tabela da análise de pareto
         def tabela_analise_pareto(colunas = ''):
             global pareto_tabela
@@ -80,6 +79,9 @@ class inicio:
                 autoalign=False,
             )
             pareto_tabela.place(relx=0.405,rely=0.45,anchor=CENTER, relheight=0.75, relwidth=0.815)
+        # Edita os valores de "ABIR TABELA"
+        def att_tabelas():
+            ep.abrir_tabela['value'] = tabelas.getTabelas('pareto')
         # Função para inserir os dados da tabela
         def analise_pareto(tabela=None, grafico=None, name=None):
             global matplot
@@ -113,6 +115,7 @@ class inicio:
             else:
                 ep.tabela_atual_var.set(value=edit_config.getTabela())
             edit_config.setIsSaved(False)
+            att_tabelas()
         # Realiza o bloqueio das entries de dados de acordo com a tabela atual
         def bloquear_entrys():
             if len(pareto_tabela.get_rows()) < 1 or len(pareto_tabela.get_columns()) == 6:
@@ -416,16 +419,19 @@ class inicio:
         ep.delete_tabela_botao.configure(command= lambda: ep.delete_tabela(pareto_tabela.destroy, 
                                                                            tabela_analise_pareto,
                                                                            att_max_att,
-                                                                           bloquear_entrys))
+                                                                           bloquear_entrys, att_tabelas))
         
         tabela_analise_pareto()
         bloquear_entrys()
+        att_tabelas()
         return analise_pareto
         
     def telas_medidas(self, tela):
         em = TbEsqueleto(tela, 'medidas', self.app, self)
         tabelas = tabela()
-        em.abrir_tabela['value'] = tabelas.getTabelas('medidas')
+        
+        def att_tabelas():
+            em.abrir_tabela['value'] = tabelas.getTabelas('medidas')
         
         def bloquear_entrys():
             if em.tabela_atual_var.get() == 'SELECIONE UMA TABELA':
@@ -446,7 +452,13 @@ class inicio:
                 delete_coluna.config(state='normal')
 
         def mudar_conj_dados():
-            atualizar_coluna['value'] = tabelas.get_TableColumns('medidas')
+            try:
+                colunas = tabelas.get_TableColumns('medidas')
+                inserir_coluna['value'] = colunas
+                atualizar_coluna['value'] = colunas
+                delete_coluna['value'] = colunas
+            except Exception as e:
+                print(e)
         #Tabela de medidas
         def tabela_medidas_matriz(colunas = ''):
             global tabela_medidas_matrix
@@ -617,10 +629,12 @@ class inicio:
             tabela_medidas_tdf.destroy()
             tabela_df =([],DataFrame([], columns=['', 'Freq. Relativa %']))
             tabela_tdf()
+            att_tabelas()
         # Insere os dados nas tabelas
         def tabelas_medidas(tabela=None, nome=None):
             importado = 1
             sqlite_table
+            att_tabelas()
             if tabela is None or isinstance(tabela, str) or tabela.empty:
                 global tabela_df
                 global tabela_matriz
@@ -716,6 +730,7 @@ class inicio:
             em.tabela_atual_var.set(value=edit_config.getTabela())
             edit_config.setIsSaved(False)
             bloquear_entrys()
+            mudar_conj_dados()
             
         #Notebok tabelas
         notebook = ttk.Notebook(tela, style='custom.TNotebook')
@@ -839,6 +854,7 @@ class inicio:
         tabela_de_medidas()
         tabela_tdf()
         bloquear_entrys()
+        att_tabelas()
         
         return tabelas_medidas
 
